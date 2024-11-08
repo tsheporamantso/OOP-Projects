2e86202c829e98ed3087b9178d87ba2b31859956
tsheporamantso <tshepo.ramantso@outlook.com>
2024-11-08 15:20:12 +0200

    Add articles dynamically

---
+++ dark-theme/app.js
@@ -0,0 +1,22 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable comma-dangle */
+/* eslint-disable import/extensions */
+import articles from './utils/data.js';
+
+const articlesContainer = document.querySelector('.articles');
+
+const articleData = articles
+  .map(
+    ({ title, date, length, snippet }) => `
+  <article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${date}</span>
+            <span>${length}</span>
+          </div>
+          <p>${snippet}</p>
+        </article>
+  `
+  )
+  .join('');
+articlesContainer.innerHTML = articleData;
