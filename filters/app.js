c4902d03e28e18f0ff2bd70829930d2f408f60af
tsheporamantso <tshepo.ramantso@outlook.com>
2024-11-11 15:36:37 +0200

    Add condition to return string if search does not match

--- filters/app.js
+++ filters/app.js
@@ -1,11 +1,23 @@
 import products from './utils/products.js';
 import getElement from './utils/getElement.js';
 
+// copy of of products array
 let filteredProducts = [...products];
 
+// element selection
 const productsContainer = getElement('.product-container');
+const form = getElement('.input-form');
+const searchInput = getElement('.search-input');
 
+// display all products
 const displayProducts = () => {
+  // no match condition
+  if (filteredProducts.length < 1) {
+    productsContainer.innerHTML = `<h6 class="text-red-600">Sorry, no products matched your search.</h6>`;
+    return;
+  }
+
+  // display all products
   productsContainer.innerHTML = filteredProducts.map(
     ({ id, image, title, price, company }) => {
       return `
@@ -25,3 +37,13 @@ const displayProducts = () => {
   );
 };
 displayProducts();
+
+// filter search
+
+form.addEventListener('keyup', () => {
+  const inputValue = searchInput.value;
+  filteredProducts = products.filter(({ title }) => {
+    return title.toLowerCase().includes(inputValue);
+  });
+  displayProducts();
+});
